046cd85769ad36bfcec96c62977ff4464dfec1c1
Aude-V <aude.vendeuvre@etu.minesparis.psl.eu>
2025-01-14 17:01:56 +0100

    inline

---
+++ intstack_inline.h
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <stdexcept>
+
+class IntStack
+{
+private:
+    int n;
+    int SIZE;
+    int *tab;
+
+public:
+    IntStack(int);
+
+    ~IntStack();
+
+    IntStack(const IntStack &r) : n(r.n), SIZE(r.SIZE)
+    {
+        tab = new int[SIZE];
+        for (int i = 0; i < n; i++)
+        {
+            tab[i] = r.tab[i];
+        }
+    }
+    IntStack &operator=(const IntStack &r)
+    {
+        if (this != &r)
+        {
+            delete[] tab;
+            tab = new int[r.SIZE];
+            for (int i = 0; i < n; i++)
+            {
+                tab[i] = r.tab[i];
+            }
+        }
+        return *this;
+    }
+
+    bool is_empty();
+    bool is_full();
+    void push(int);
+    int get_top();
+    int pop();
+    void print();
+};
+
+
+inline IntStack::IntStack(int s) : n(0), SIZE(s)
+{
+    if (SIZE < 0)
+    {
+        std::cout << "taille non valide" << std::endl;
+        exit(0);
+    }
+    else
+    {
+        tab = new int[SIZE];
+    }
+}
+
+inline IntStack::~IntStack()
+{
+    delete[] tab;
+}
+
+inline bool IntStack::is_empty()
+{
+    return n == 0;
+}
+
+inline bool IntStack::is_full()
+{
+    return n == SIZE;
+}
+
+inline void IntStack::push(int e)
+{
+    if (is_full())
+    {
+        std::cout << "la pile est pleine\n";
+        exit(0);
+    }
+    tab[n] = e;
+    n = n + 1;
+}
+
+inline int IntStack::get_top()
+{
+    if (is_empty())
+    {
+        std::cout << "la pile est vide\n";
+        return 0;
+    }
+    else
+    {
+        return tab[n - 1];
+    }
+}
+
+inline int IntStack::pop()
+{
+    if (is_empty())
+    {
+        std::cout << "la pile est vide\n";
+        return 0;
+    }
+    else
+    {
+        int elem = tab[n - 1];
+        n = n - 1;
+        return elem;
+    }
+}
+
+inline void IntStack::print()
+{
+    std::cout << '[';
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << tab[i] << ' ';
+    }
+    std::cout << "]\n";
+}
+
+int main()
+{
+    int size;
+    std::cin >> size;
+    IntStack s(size);
+    s.print(); // affichera []
+    s.pop();
+    std::cout << s.is_empty() << std::endl;
+    s.push(1);
+    std::cout << s.is_empty() << std::endl;
+    s.print();
+    s.push(2);
+    s.push(3);
+    std::cout << s.is_full() << std::endl;
+    s.print();                         // affichera [1 2 3 ]
+    std::cout << s.pop() << std::endl; // affichera 3
+    s.print();                         // affichera [1 2 ]
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    std::cout << s.is_full() << std::endl;
+    s.print();
+    return 0;
+}
